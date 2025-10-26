8f3c1dede5a2e0e29cfa115f7eeb31bea60a41b0
dominik strabel <d.strabel.527.ug.edu.pl>
2025-10-26 15:47:41 +0100

    z3

---
+++ lab03/zadanie3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+
+
+// wyznaczam dwie liczby 
+int a;
+int b; 
+printf("podaj dwie liczby całkowite: ");
+scanf("%d %d", &a, &b);
+if(a > b){
+   printf("a\n");
+} 
+else{
+    printf("b\n");
+} 
+
+
+
+
+
+
+}
